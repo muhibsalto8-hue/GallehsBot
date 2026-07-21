5991350599c7f143bd23c05dd5876eac8e26f09a
muhibsalto8-hue <muhibsalto8@gmail.com>
2026-07-21 10:12:44 +0700

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,19 @@
+const config = {
+  botName: "GallehsBot",
+  ownerName: "Gallehs",
+  ownerNumber: "6289663117070",
+
+  prefix: ".",
+
+  sessionName: "session",
+
+  menu: {
+    image: "./assets/menu.jpg",
+    audio: "./assets/menu.mp3",
+    audioEnable: true
+  },
+
+  database: "./database/database.json"
+}
+
+export default config
