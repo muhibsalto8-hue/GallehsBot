b9eb3a9a7e4685b03d8d3a8515226e7508b57dfb
muhibsalto8-hue <muhibsalto8@gmail.com>
2026-07-21 14:30:09 +0700

    Update ttmp4.js

--- plugins/ttmp4.js
+++ plugins/ttmp4.js
@@ -3,8 +3,8 @@ export const command = ["ttmp4"]
 export async function run(sock, m, args) {
     const jid = m.key.remoteJid
 
-    if (!args[0]) {
-        return await sock.sendMessage(jid, {
+    if (!args.length) {
+        return sock.sendMessage(jid, {
             text: "❌ Contoh:\n.ttmp4 https://vt.tiktok.com/xxxx"
         })
     }
@@ -12,8 +12,20 @@ export async function run(sock, m, args) {
     const url = args[0]
 
     await sock.sendMessage(jid, {
-        text: "⏳ Sedang mengambil video TikTok..."
+        text: "⏳ Mengambil video TikTok..."
     })
 
-    // API downloader dipanggil di sini
+    try {
+
+        // TODO: Ambil video dari API
+
+        await sock.sendMessage(jid, {
+            text: "Plugin belum dihubungkan ke API downloader."
+        })
+
+    } catch (e) {
+        await sock.sendMessage(jid, {
+            text: "❌ Gagal mengambil video."
+        })
+    }
 }
