8e2a90a4ebd2d083321cded726ff04d20b5c0e3e
muhibsalto8-hue <muhibsalto8@gmail.com>
2026-07-21 10:33:51 +0700

    Modify ping command to include status message
    Update ping command response message.

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,9 +1,7 @@
 export const command = ["ping"]
 
 export async function run(sock, m) {
-  const from = m.key.remoteJid
-
-  await sock.sendMessage(from, {
-    text: "🏓 Pong!"
-  })
+    await sock.sendMessage(m.chat, {
+        text: "🏓 Pong!\nBot berjalan dengan normal."
+    })
 }
